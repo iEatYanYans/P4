4fa493a0201c40f480bce662084451e8aa42610e
Yan <Yan@Yan.local>
2016-12-19 16:50:36 -0500

    changed the welcome page image & small fixes

--- resources/views/tracker/history.blade.php
+++ resources/views/tracker/history.blade.php
@@ -8,10 +8,9 @@
     <h2> All Entries </h2><br>
     @foreach ($entries as $entry)
       <div class = "entry">
-          <h3 class= 'accordion-toggle'><span><button class= 'accordion'><b class='caret'></b> {{substr($entry->time_woken, 0, 10)}} </h3></button>
+          <button class= 'accordion'><h3 class= 'accordion-toggle'><span><b class='caret'></b> {{substr($entry->time_woken, 0, 10)}}
             <a href= '/edit/{{$entry->id}}'><img alt='edit' src='/edit-icon.png' width='25' height= '25'></a>
-            <a href= '/delete/{{$entry->id}}'><img alt='delete' src='/delete-icon.png' width='25' height='25'></a>
-          </span>
+            <a href= '/delete/{{$entry->id}}'><img alt='delete' src='/delete-icon.png' width='25' height='25'></a></h3></button></span>
           <div class= 'accordion-panel'>
             <br><p> <b>Slept at:</b>   {{$entry -> time_slept}}<br>
             <b>Woke up:</b>   {{$entry->time_woken}}<br>
