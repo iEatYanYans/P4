ba955a2cc0fe499051e13fc6e5fa032319b2008a
Yan <Yan@Yan.local>
2016-12-19 16:41:21 -0500

    changed the welcome page image & small fixes

--- resources/views/tracker/history.blade.php
+++ resources/views/tracker/history.blade.php
@@ -8,10 +8,10 @@
     <h2> All Entries </h2><br>
     @foreach ($entries as $entry)
       <div class = "entry">
-          <span><button class= 'accordion'><h3 class= 'accordion-toggle'><b class='caret'></b> {{substr($entry->time_woken, 0, 10)}} </h3>
-            <a href= '/edit/{{$entry->id}}'><img alt='edit' src='/edit-icon.png' width='25' height= '25'></img></a>
-            <a href= '/delete/{{$entry->id}}'><img alt='delete' src='/delete-icon.png' width='25' height='25'></img></a>
-          </button></span>
+          <span><button class= 'accordion'><h3 class= 'accordion-toggle'><b class='caret'></b> {{substr($entry->time_woken, 0, 10)}} </h3></button>
+            <a href= '/edit/{{$entry->id}}'><img alt='edit' src='/edit-icon.png' width='25' height= '25'></a>
+            <a href= '/delete/{{$entry->id}}'><img alt='delete' src='/delete-icon.png' width='25' height='25'></a>
+          </span>
           <div class= 'accordion-panel'>
             <br><p> <b>Slept at:</b>   {{$entry -> time_slept}}<br>
             <b>Woke up:</b>   {{$entry->time_woken}}<br>
